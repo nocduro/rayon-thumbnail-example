301e58b458cc41adb86c5c66487ce2f43a0e911f
Mackenzie Hauck <mhauck@live.ca>
2017-08-28 22:24:43 -0600

    Add "error handling" to parallel code

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,12 @@ use rayon::prelude::*;
 fn find_jpg() -> Vec<PathBuf> {
     let mut paths = Vec::new();
 
-    for entry in glob("**/*.jpg").expect("Failed to read glob pattern") {
+    for entry in glob("*.jpg").expect("Failed to read glob pattern") {
         match entry {
             Ok(path) => {
                 if let Some(parent) = path.parent() {
-                    // skip already generated thumbnails
                     if parent.ends_with("thumbs") {
+                        // skip already generated thumbnails
                         continue;
                     }
                 }
@@ -31,43 +31,58 @@ fn find_jpg() -> Vec<PathBuf> {
     paths
 }
 
-fn resize_jpg(file: &Path, longest_edge: u32) -> ImageResult<()> {
+fn resize_jpg(file: &Path, thumb_dir: &Path, longest_edge: u32) -> ImageResult<()> {
+    fs::create_dir_all(thumb_dir)?;
     let img = image::open(file)?;
-    let img = img.resize(longest_edge, longest_edge, FilterType::Lanczos3);
+    let img = img.resize(longest_edge, longest_edge, FilterType::Nearest);
 
-    let output_path = Path::new("thumbs").join(file);
+    let output_path = thumb_dir.join(file);
 
     let ref mut fout = File::create(output_path.as_path())?;
     let _ = img.save(fout, image::JPEG)?;
 
     Ok(())
 }
-
+/*
 fn sequential(files: &[PathBuf], longest_edge: u32) -> ImageResult<()> {
-    fs::create_dir_all("thumbs").expect("Couldn't make thumbnail directory");
+    fs::create_dir_all("thumbs")?;
 
     for jpg in files.iter() {
         resize_jpg(jpg, longest_edge)?;
     }
     Ok(())
+}*/
+
+#[derive(Debug)]
+struct ImageProblem {
+    file_path: PathBuf,
+    error: image::ImageError,
 }
 
-fn parallel(files: &[PathBuf], longest_edge: u32) -> ImageResult<()> {
-    fs::create_dir_all("thumbs")?;
-
+fn parallel(files: &[PathBuf], thumb_dir: &Path, longest_edge: u32) -> Vec<ImageProblem> {
+    //fs::create_dir_all("thumbs").map_err(|e| ImageProblem{file_path: "thumbs", error: e});
     files
         .par_iter()
-        .for_each(|jpg| { resize_jpg(jpg, longest_edge).unwrap(); });
-
-    Ok(())
+        .map(|path| {
+            resize_jpg(path, thumb_dir, longest_edge).map_err(|e| {
+                ImageProblem {
+                    file_path: path.clone(),
+                    error: e,
+                }
+            })
+        })
+        .filter_map(|x| x.err())
+        .collect()
 }
+
+
+
 
 fn main() {
     let files = find_jpg();
+    println!("Found {} files to convert", files.len());
 
-    match sequential(&files, 300) {
-        Ok(()) => println!("{} thumbnails saved", files.len()),
-        Err(e) => println!("Failed to create all thumbnails: {:?}", e),
+    for problem in parallel(&files, Path::new("thumbnails"), 300) {
+        println!("{:?} failed with error: {}", problem.file_path, problem.error);
     }
-
 }
